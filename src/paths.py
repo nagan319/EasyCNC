2cad45f1df304f3356d26c14589a83c434f3f15b
nagan319 <sashanaganov@gmail.com>
2024-06-27 16:50:39 -0700

    create blank dirs

--- src/paths.py
+++ src/paths.py
@@ -15,16 +15,34 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 DATABASE_URI = f'sqlite:///{os.path.join(DATA_DIR, "app_data.db")}'
 
 LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
+if not os.path.exists(LOGS_DIR):
+    os.makedirs(LOGS_DIR)
 
 """
 Temporary directory paths
 """
 
+''' these must be created when downloading from github '''
 CACHE_DIR = os.path.join(DATA_DIR, 'cache')
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
+
 IMAGE_PREVIEW_PATH = os.path.join(CACHE_DIR, 'image preview')
+if not os.path.exists(IMAGE_PREVIEW_PATH):
+    os.makedirs(IMAGE_PREVIEW_PATH)
+
 PART_PREVIEW_PATH = os.path.join(CACHE_DIR, 'part preview')
+if not os.path.exists(PART_PREVIEW_PATH):
+    os.makedirs(PART_PREVIEW_PATH)
+
 PLATE_PREVIEW_PATH = os.path.join(CACHE_DIR, 'plate preview')
+if not os.path.exists(PLATE_PREVIEW_PATH):
+    os.makedirs(PLATE_PREVIEW_PATH)
+
 ROUTER_PREVIEW_PATH = os.path.join(CACHE_DIR, 'router preview')
+if not os.path.exists(ROUTER_PREVIEW_PATH):
+    os.makedirs(ROUTER_PREVIEW_PATH)
+
 USER_SETTINGS_PATH = os.path.join(DATA_DIR, 'user_settings.json')
 
 """
